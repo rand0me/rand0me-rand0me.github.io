dfbd03fefd536efd6aacce378f773ee17b78b36d
Daniil Kolesnik <dandydan2k@gmail.com>
2019-07-23 16:59:28 -0400

    Add README.md generation

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,5 +5,6 @@ const fs = require("fs");
 const buildPath = path.resolve(__dirname, "../build");
 
 fs.writeFileSync(buildPath + "/CNAME", "rand0.me");
+fs.writeFileSync(buildPath + "/README.md", "# SEE SOURCES :rocket: [HERE](https://github.com/rand0me/rand0me.github.io/tree/source)")
 
 pages.publish(buildPath, { branch: "master" });
